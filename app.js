1e8f76753fe68fdb04e94239730350e48664c3ae
MarioErnst <121234721+MarioErnst@users.noreply.github.com>
2025-02-25 13:35:21 -0300

    add_files

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('saludoBtn').addEventListener('click', function() {
+    document.getElementById('saludo').textContent = '¡Hola! Este es un saludo dinámico desde app.js';
+  });
+  
